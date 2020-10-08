468ff7743ba7b423cd0b06387fb1e19af57c0938
Matthew Richardson <mattcolerichardson@gmail.com>
2020-10-08 17:23:32 -0500

    Looked up someone elses project and looks like heroku needs the directory this way app.use(express.static(path.join(__dirname, '../Public/Controller')));

--- ServerSide/Index.js
+++ ServerSide/Index.js
@@ -8,8 +8,8 @@ app.set('view engine', 'pug');
 app.listen(process.env.PORT || 3000, () => console.log('listening at 3000'));
 
 //app.use(express.static('./View/*'));
-app.use(express.static(path.join(__dirname, '/Public/View/')));
-app.use(express.static(path.join(__dirname, '/Public/Controller/')));
+app.use(express.static(path.join(__dirname, '../Public/View')));
+app.use(express.static(path.join(__dirname, '../Public/Controller')));
 //app.use(express.static(__dirname + '/Public/View/'));
 //app.use(express.static(__dirname + '/Public/Controller/'));
 
