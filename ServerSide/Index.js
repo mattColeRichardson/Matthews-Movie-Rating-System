78789e2540b9f95058a829181aeb695ed044b3c6
Matthew Richardson <mattcolerichardson@gmail.com>
2020-10-07 20:11:07 -0500

    Setup the path to make node host the right files

--- ServerSide/Index.js
+++ ServerSide/Index.js
@@ -5,8 +5,9 @@ const app = express();
 
 app.listen(process.env.PORT || 3000, () => console.log('listening at 3000'));
 
-app.use(express.static('/Public/View'));
-app.use(express.static('/Public/Controller'));
+//app.use(express.static('./View/*'));
+app.use(express.static('./ServerSide/Public/View/'));
+app.use(express.static('./ServerSide/Public/Controller/'));
 
 app.get('/Search&t=[a-z]*', (req,res) =>
 {
