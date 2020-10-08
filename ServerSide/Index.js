5c0fd14db5b858e2e4aa9593852646b331c4e084
Matthew Richardson <mattcolerichardson@gmail.com>
2020-10-08 16:56:46 -0500

    needed to impor the path cosnst to get the dir name correct.

--- ServerSide/Index.js
+++ ServerSide/Index.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const ImdbApi = require('./Model/ImdbAPI');
 const imdb = new ImdbApi();
@@ -7,8 +8,10 @@ app.set('view engine', 'pug');
 app.listen(process.env.PORT || 3000, () => console.log('listening at 3000'));
 
 //app.use(express.static('./View/*'));
-app.use(express.static(__dirname + '/Public/View/'));
-app.use(express.static(__dirname + '/Public/Controller/'));
+app.use(express.static(path.join(__dirname, '/Public/View/')));
+app.use(express.static(path.join(__dirname, '/Public/Controller/')));
+//app.use(express.static(__dirname + '/Public/View/'));
+//app.use(express.static(__dirname + '/Public/Controller/'));
 
 app.get('/Search&t=[a-z]*', (req,res) =>
 {
